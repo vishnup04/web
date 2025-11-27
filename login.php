4b53787c584fdcfacdc015625d17c7cdfe06f835
vishnup04 <vishnupyadav23@gmail.com>
2025-11-27 11:29:27 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "anseera");
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM login";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    echo "<table border='1'>";
+    echo "<tr><th>ID</th><th>Name</th><th>Email</th><th>Number</th></tr>";
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>
+                <td>".$row['id']."</td>
+                <td>".$row['name']."</td>
+                <td>".$row['email']."</td>
+                <td>".$row['number']."</td>
+              </tr>";
+    }
+
+    echo "</table>";
+} else {
+    echo "No data found";
+}
+
+mysqli_close($conn);
+?>
---
+++ student.php
@@ -0,0 +1,22 @@
+<?php 
+$students = array("ANSI", "AMANI", "HAWA", "FIDA", "SHAHMA"); 
+ 
+echo "<h3>Original Student List:</h3>"; 
+echo "<pre>"; 
+print_r($students); 
+echo "</pre>"; 
+ 
+asort($students); 
+ 
+echo "<h3>Sorted Student List (Ascending):</h3>"; 
+echo "<pre>"; 
+print_r($students); 
+echo "</pre>"; 
+ 
+arsort($students); 
+ 
+echo "<h3>Sorted Student List (Descending):</h3>"; 
+echo "<pre>"; 
+print_r($students); 
+echo "</pre>"; 
+?> 
